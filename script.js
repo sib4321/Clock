3bbb382f816064b24c5401f87521c4e4a28122b2
sib4321 <sib4321@ukr.net>
2017-10-02 14:55:03 +0300

    Get the curent date

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ let hrPosition = 20;
 let minPosition = 130;
 let secPosition = 267;
 
+var date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
+
+
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
 SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
